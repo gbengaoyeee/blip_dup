99dd655857eff222584ec0857609673a55142c7d
Me <Srikanth@srikanths-imac-2.local>
2018-04-08 16:40:22 -0400

    trying to do optimization routes

--- blip/Blip/Controllers/CustomFiles/PickUpAnnotation.swift
+++ blip/Blip/Controllers/CustomFiles/PickUpAnnotation.swift
@@ -9,10 +9,38 @@
 import UIKit
 import Mapbox
 
-class PickUpAnnotation: BlipAnnotation{
+class PickUpAnnotation: MGLPointAnnotation{
     
-    override init(coordinate: CLLocationCoordinate2D, title: String?, subtitle: String?) {
-        super.init(coordinate: coordinate, title: title, subtitle: subtitle)
+    var photoURL: URL?
+    var image:UIImage?
+    var reuseIdentifier: String?
+    
+    var observers: NSMutableSet! = NSMutableSet()
+    
+    init(coordinate: CLLocationCoordinate2D, title: String?, subtitle: String?) {
+        super.init()
+        super.coordinate = coordinate
+        super.title = title
+        super.subtitle = subtitle
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+    }
+    
+    override func addObserver(_ observer: NSObject, forKeyPath keyPath: String, options: NSKeyValueObservingOptions = [], context: UnsafeMutableRawPointer?) {
+        let observerId : String = "\(observer.hashValue)\(keyPath)"
         
+        self.observers.add(observerId)
+        super.addObserver(observer, forKeyPath: keyPath, options: options, context: context)
+    }
+    
+    override func removeObserver(_ observer: NSObject, forKeyPath keyPath: String, context: UnsafeMutableRawPointer?) {
+        let observerId : String = "\(observer.hashValue)\(keyPath)"
+        
+        if (self.observers.contains(observerId)) {
+            self.observers.remove(observerId)
+            super.removeObserver(observer, forKeyPath: keyPath)
+        }
     }
 }
