c946d473ec3ed2e6192a8dbfd590a2fce73ef40b
Me <Srikanth@srikanths-imac-2.local>
2018-04-09 12:27:05 -0400

    geoJson

--- blip/Blip/Controllers/ViewControllers/ViewMapOfJobsVC.swift
+++ blip/Blip/Controllers/ViewControllers/ViewMapOfJobsVC.swift
@@ -86,45 +86,66 @@ extension ViewMapOfJobsVC: MGLMapViewDelegate{
         }
     }
     
-    func calculateRoute(waypoints: [Waypoint],
-                        completion: @escaping (Route?, Error?) -> ()) {
-        
-        // Coordinate accuracy is the maximum distance away from the waypoint that the route may still be considered viable, measured in meters. Negative values indicate that a indefinite number of meters away from the route and still be considered viable.
-        let startPoint = Waypoint(coordinate: currentLocation, coordinateAccuracy: -1, name: "Origin")
-        var waypointsWithCurrentLoc = waypoints
-        waypointsWithCurrentLoc.insert(startPoint, at: 0)
-        // Specify that the route is intended for automobiles avoiding traffic
-        let options = NavigationRouteOptions(waypoints: waypointsWithCurrentLoc, profileIdentifier: .automobileAvoidingTraffic)
-        
-        // Generate the route object and draw it on the map
-        _ = Directions.shared.calculate(options) { [unowned self] (waypoints, routes, error) in
-            self.directionsRoute = routes?.first
-            // Draw the route on the map after creating it
-            self.drawRoute(route: self.directionsRoute!)
+//    func calculateRoute(waypoints: [Waypoint],
+//                        completion: @escaping (Route?, Error?) -> ()) {
+//
+//        // Coordinate accuracy is the maximum distance away from the waypoint that the route may still be considered viable, measured in meters. Negative values indicate that a indefinite number of meters away from the route and still be considered viable.
+//        let startPoint = Waypoint(coordinate: currentLocation, coordinateAccuracy: -1, name: "Origin")
+//        var waypointsWithCurrentLoc = waypoints
+//        waypointsWithCurrentLoc.insert(startPoint, at: 0)
+//        // Specify that the route is intended for automobiles avoiding traffic
+//        let options = NavigationRouteOptions(waypoints: waypointsWithCurrentLoc, profileIdentifier: .automobileAvoidingTraffic)
+//
+//        // Generate the route object and draw it on the map
+//        _ = Directions.shared.calculate(options) { [unowned self] (waypoints, routes, error) in
+//            self.directionsRoute = routes?.first
+//            // Draw the route on the map after creating it
+//            self.drawRoute(route: self.directionsRoute!)
+//        }
+//    }
+    
+    func drawRoute(data: Data) {
+        do {
+            // Convert the file contents to a shape collection feature object
+            let shapeCollectionFeature = try MGLShape(data: data, encoding: String.Encoding.utf8.rawValue) as! MGLPolylineFeature
+            
+            let polyline = shapeCollectionFeature
+                // Optionally set the title of the polyline, which can be used for:
+                //  - Callout view
+                //  - Object identification
+            polyline.title = polyline.attributes["name"] as? String
+                // Add the annotation on the main thread
+            DispatchQueue.main.async(execute: {
+                    // Unowned reference to self to prevent retain cycle
+                [unowned self] in
+                self.map.addAnnotation(polyline)
+            })
+        }
+        catch {
+            print("GeoJSON parsing failed")
         }
     }
     
-    func drawRoute(route: Route) {
-        guard route.coordinateCount > 0 else { return }
-        // Convert the route’s coordinates into a polyline
-        var routeCoordinates = route.coordinates!
-        let polyline = MGLPolylineFeature(coordinates: &routeCoordinates, count: route.coordinateCount)
-        
-        // If there's already a route line on the map, reset its shape to the new route
-        if let source = map.style?.source(withIdentifier: "route-source") as? MGLShapeSource {
-            source.shape = polyline
-        } else {
-            let source = MGLShapeSource(identifier: "route-source", features: [polyline], options: nil)
-            
-            // Customize the route line color and width
-            let lineStyle = MGLLineStyleLayer(identifier: "route-style", source: source)
-            lineStyle.lineColor = MGLStyleValue(rawValue: #colorLiteral(red: 0.2796384096, green: 0.4718205929, blue: 1, alpha: 1))
-            lineStyle.lineWidth = MGLStyleValue(rawValue: 8)
-            
-            // Add the source and style layer of the route line to the map
-            map.style?.addSource(source)
-            map.style?.addLayer(lineStyle)
+    func mapView(_ mapView: MGLMapView, lineWidthForPolylineAnnotation annotation: MGLPolyline) -> CGFloat {
+        // Set the line width for polyline annotations
+        return 6.0
+    }
+    
+    func mapView(_ mapView: MGLMapView, strokeColorForShapeAnnotation annotation: MGLShape) -> UIColor {
+        // Give our polyline a unique color by checking for its `title` property
+        if (annotation.title == "Crema to Council Crest" && annotation is MGLPolyline) {
+            // Mapbox cyan
+            return UIColor(red: 59/255, green:178/255, blue:208/255, alpha:1)
         }
+        else
+        {
+            return UIColor.green
+        }
+    }
+    
+    func mapView(_ mapView: MGLMapView, alphaForShapeAnnotation annotation: MGLShape) -> CGFloat {
+        // Set the alpha for all shape annotations to 1 (full opacity)
+        return 1
     }
     
     func mapView(_ mapView: MGLMapView, didSelect annotation: MGLAnnotation) {
@@ -133,28 +154,32 @@ extension ViewMapOfJobsVC: MGLMapViewDelegate{
             
             var waypointList = [Waypoint]()
             MyAPIClient.sharedClient.optimizeRoute(locations: (castedAnnotation.job?.locList)!, completion: { (data) in
-                let arrayOfWaypoints = data!["waypoints"] as! [[String:Any]]
-            
-                for way in arrayOfWaypoints{
-                    
-                    let name = (way["name"] as! String)
-                    let longitude = (way["location"] as! [Double])[0]
-                    let latitude = (way["location"] as! [Double])[1]
-                    let coordinate = CLLocationCoordinate2D(latitude: latitude, longitude: longitude)
-                    let waypoint = Waypoint(coordinate: coordinate, coordinateAccuracy: -1, name: name)
-                    waypointList.insert(waypoint, at: (way["waypoint_index"] as! Int))
-                }
-                print(waypointList, "This is the waypoint list")
+//                var ind = 0
+//                while ind < (castedAnnotation.job?.locList.count)!{
+//
+//                    for way in arrayOfWaypoints{
+//
+//                        if (way["waypoint_index"] as! Int) == ind{
+//                            print("found index:", ind)
+//                            let name = (way["name"] as! String)
+//                            let longitude = (way["location"] as! [Double])[0]
+//                            let latitude = (way["location"] as! [Double])[1]
+//                            let coordinate = CLLocationCoordinate2D(latitude: latitude, longitude: longitude)
+//                            let waypoint = Waypoint(coordinate: coordinate, coordinateAccuracy: -1, name: name)
+//                            waypointList.append(waypoint)
+//                            ind += 1
+//                        }
+//                    }
+//                }
+                
+                print(data, "This is the waypoint list")
                 mapView.removeAnnotations(mapView.annotations!)
                 mapView.addAnnotation(annotation)
                 let pickupAnnotation = BlipAnnotation(coordinate: (castedAnnotation.job?.pickupLocationCoordinates)!, title: "Pickup Point", subtitle: nil)
                 mapView.addAnnotation(pickupAnnotation)
                 mapView.showAnnotations([annotation, pickupAnnotation, self.map.userLocation!], animated: true)
-                self.calculateRoute(waypoints: waypointList, completion: { (route, error) in
-                    if error != nil{
-                        print("Error calculating route to pickup point")
-                    }
-                })
+                // if let here 
+                self.drawRoute(data: data!)
             })
         }
     }
