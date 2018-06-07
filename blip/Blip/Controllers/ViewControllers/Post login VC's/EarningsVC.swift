951aabc0dd756c689f7c4d2b56bcb97aca2c31d8
Srikanth Srinivas <Srikanth@Srikanths-iMac-2.local>
2018-06-06 22:53:49 -0400

    Commit for version 0.9.0

--- blip/Blip/Controllers/ViewControllers/Post login VC's/EarningsVC.swift
+++ blip/Blip/Controllers/ViewControllers/Post login VC's/EarningsVC.swift
@@ -8,19 +8,20 @@
 
 import UIKit
 import Material
+import Lottie
 
 class EarningsVC: UIViewController {
 
     @IBOutlet weak var earningsLabel: UILabel!
     @IBOutlet weak var depositDateLabel: UILabel!
-    @IBOutlet weak var feedbackText: UITextView!
     @IBOutlet weak var goHomeButton: RaisedButton!
+    @IBOutlet weak var checkView: UIView!
     
     var job: Job!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        prepareLabels()
         // Do any additional setup after loading the view.
     }
 
@@ -29,13 +30,23 @@ class EarningsVC: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+    override func viewWillAppear(_ animated: Bool) {
+        prepareCheckView()
+    }
+    
     override func viewWillLayoutSubviews() {
         prepareGoHomeButton()
     }
     
     func prepareGoHomeButton(){
-        goHomeButton.setIcon(icon: .googleMaterialDesign(.check), iconSize: self.goHomeButton.frame.size.width, color: UIColor.white, backgroundColor: UIColor.clear, forState: .normal)
+        goHomeButton.setIcon(icon: .googleMaterialDesign(.arrowForward), iconSize: (0.5*self.goHomeButton.frame.size.width), color: UIColor.white, backgroundColor: #colorLiteral(red: 0.3037296832, green: 0.6713039875, blue: 0.9027997255, alpha: 1), forState: .normal)
         goHomeButton.makeCircular()
+    }
+    
+    func prepareCheckView(){
+        let check = LOTAnimationView(name: "checkmark")
+        checkView.handledAnimation(Animation: check, width: 2.0, height: 2.0)
+        check.play()
     }
     
     func prepareLabels(){
