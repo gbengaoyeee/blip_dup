7777b03672edbb14a732a03b3221efacae481c44
Gbenga Ayobami <Ayo@Srikanths-MacBook-Air.local>
2018-06-10 18:45:43 -0400

    cbp

--- blip/Blip/Controllers/ViewControllers/AppFABMenuController.swift
+++ blip/Blip/Controllers/ViewControllers/AppFABMenuController.swift
@@ -28,6 +28,7 @@ class AppFABMenuController: FABMenuController{
         prepareFABButton()
         prepareLogoutFabMenuItem()
         prepareProfilePageFabMenuItem()
+        preparePrivacy()
         prepareFABMenu()
     }
 }
